1d948e35ce3d2f2b445a272ad0ee9c9715f789dd
Lee <849982480@qq.com>
2017-10-21 10:19:42 +0800

    v0.3.1

--- src/main/java/edu/zust/survey/interceptor/StudentInterceptor.java
+++ src/main/java/edu/zust/survey/interceptor/StudentInterceptor.java
@@ -1,7 +1,6 @@
 package edu.zust.survey.interceptor;
 
 import edu.zust.survey.common.Const;
-import edu.zust.survey.entity.Manager;
 import edu.zust.survey.entity.Student;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,14 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Created by Lee on 2017/10/19.
+ * Created by Lee on 2017/10/20.
  */
-public class ManagerInterceptor implements HandlerInterceptor{
+public class StudentInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         HttpSession session = httpServletRequest.getSession();
         Object object = session.getAttribute(Const.CURRENT_USER);
-        System.out.println("!!");
-        if (object instanceof Manager){
+        if (object instanceof Student){
             return true;
         } else {
             httpServletResponse.sendRedirect("/login.html");
